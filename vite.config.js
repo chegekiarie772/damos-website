d91ca6ccc265d3d83c8ea6a02955a38798dbae75
chege772 <chegekiarie772@gmail.com>
2026-05-25 20:52:22 +0300

    Add package.json and dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // This is the most important part for local files
   plugins: [react()],
+  base: './',
 })
